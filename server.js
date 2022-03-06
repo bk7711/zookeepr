ad0d7edb946e6c95d833b29488c4f49565866f39
Carolyn B <bmathematics@gmail.com>
2022-03-06 15:31:21 -0500

    add id parameter search and error handling

--- server.js
+++ server.js
@@ -39,7 +39,10 @@ function filterByQuery(query, animalsArray){
     return filteredResults;
 }
 
-
+function findById(id, animalsArray){
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
 app.get('/api/animals', (req,res) => {
     let results = animals;
@@ -47,6 +50,16 @@ app.get('/api/animals', (req,res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(animals);
+});
+
+app.get('/api/animals/:id', (req,res) => {
+    const result = findById(req.params.id, animals);
+    if(result){
+        res.json(result);
+    }else{
+        res.send(404);
+    }
+    
 });
 
 app.listen(PORT, () => {
